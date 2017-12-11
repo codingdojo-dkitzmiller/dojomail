dbfbeeef0ddc7c2b9b3a5302fd8e4b3ce52837fc
Dave Kitzmiller <codingdojo.dkitzmiller@gmail.com>
2017-12-11 00:30:18 -0800

    us-timezone-display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { DojomailComponent } from './dojomail/dojomail.component';
 import { RetroBarcodeGeneratorComponent } from './retro-barcode-generator/retro-barcode-generator.component';
 import {RandomColorsService} from './service/random-colors.service';
+import { UsTimezoneDisplayComponent } from './us-timezone-display/us-timezone-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DojomailComponent,
-    RetroBarcodeGeneratorComponent
+    RetroBarcodeGeneratorComponent,
+    UsTimezoneDisplayComponent
   ],
   imports: [
     BrowserModule
