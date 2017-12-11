b39347ea75f700f79bdb3180dfaca9cf0146adc7
Dave Kitzmiller <codingdojo.dkitzmiller@gmail.com>
2017-12-10 20:45:06 -0800

    retro barcode generator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DojomailComponent } from './dojomail/dojomail.component';
+import { RetroBarcodeGeneratorComponent } from './retro-barcode-generator/retro-barcode-generator.component';
+import {RandomColorsService} from './service/random-colors.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DojomailComponent
+    DojomailComponent,
+    RetroBarcodeGeneratorComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [RandomColorsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
